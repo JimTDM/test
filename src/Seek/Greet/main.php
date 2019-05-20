6829e6153ffd2624beed424dd2491b0626cec263
JimTDM <jijeffrey55@gmail.com>
2019-05-20 23:31:14 +0800

    Add files via upload

---
+++ src/Seek/Greet/main.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Seek\Greet;
+
+use pocketmine\command\Command;
+use pocketmine\command\ComamndSender;
+use pocketmine\plugin\PluginBase;
+class MyPlugin extends PluginBase{
+}
+
+class Greet extends PluginBase;
+
+public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+    $commandName = $command->getName();
+    if($commandName === "greet"){
+      $sender->sendMessage("Hi!");
+      return true;
+    }
+    return false;
+  }
