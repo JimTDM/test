6d0eb4e3cedec1480a91d3f4e949e79445854c1c
Seeker-Devs <49820135+Seeker-Devs@users.noreply.github.com>
2019-05-20 21:01:26 +0500

    Update main.php

--- src/Seek/Greet/main.php
+++ src/Seek/Greet/main.php
@@ -8,6 +8,8 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase;
 
+public function onEnable(){
+    }
 public function onCommand(CommandSender $sender, Command $command, $label, array $args){
     $commandName = $command->getName();
     if($commandName === "greet"){
