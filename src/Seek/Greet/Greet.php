16233c64fcb6f3a5059413ec92009a4f5c73ea92
Seeker-Devs <49820135+Seeker-Devs@users.noreply.github.com>
2019-05-21 16:36:57 +0500

    Fixed errors

--- src/Seek/Greet/Greet.php
+++ src/Seek/Greet/Greet.php
@@ -3,18 +3,19 @@
 namespace Seek\Greet;
 
 use pocketmine\command\Command;
-use pocketmine\command\ComamndSender;
+use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 
 class Greet extends PluginBase{
 
 public function onEnable(){
     }
-public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+public function onCommand(CommandSender $sender, Command $command, $label, array $args) :bool{
     $commandName = $command->getName();
     if($commandName === "greet"){
       $sender->sendMessage("Hi!");
       return true;
     }
     return false;
+  }
   }
